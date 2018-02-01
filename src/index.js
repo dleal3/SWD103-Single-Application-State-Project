96c10cc042a7a5aa45bef72194ec59d48d9235fc
dleal3 <dray364@gmail.com>
2018-02-01 00:48:00 -0600

    Completed Single App Project 1

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const arr = ['Bill', 'Joe', 'Emily', 'Andrea']
+const newStudents = ['Andrew', 'Tasha', 'Carol', 'George'];
+
+
+const totalStudents = [...arr, ...newStudents];
+
+
+
+const [a, b] = totalStudents;
